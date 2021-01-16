f3f09ee9aa6d69b917da998ffe94cef467c60ca2
Daniel151505 <daniel150520011@gmail.com>
2021-01-16 17:01:01 -0500

    Agregando Metodo Eliminar

--- aplicacionPresupuesto/src/app/ingreso/ingreso.service.ts
+++ aplicacionPresupuesto/src/app/ingreso/ingreso.service.ts
@@ -9,4 +9,11 @@ export class IngresoService {
         new Ingreso('Salario',4000),
         new Ingreso('Venta de auto', 500)
     ];
+
+    //Metodo Eliminar
+    
+    eliminar(ingreso:Ingreso){
+        const indice: number = this.ingresos.indexOf(ingreso)
+        this.ingresos.splice(indice,1);
+    }
 }
