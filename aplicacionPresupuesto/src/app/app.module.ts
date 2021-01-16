85e41664229311a6fb3a282e110810ba1f58a44e
Daniel151505 <daniel150520011@gmail.com>
2021-01-16 15:28:30 -0500

    Declarando y Importando los Componentes creados

--- aplicacionPresupuesto/src/app/app.module.ts
+++ aplicacionPresupuesto/src/app/app.module.ts
@@ -4,11 +4,17 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CabeceroComponent } from './cabecero/cabecero.component';
+import { IngresoComponent } from './ingreso/ingreso.component';
+import { EgresoComponent } from './egreso/egreso.component';
+import { FormularioComponent } from './formulario/formulario.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CabeceroComponent
+    CabeceroComponent,
+    IngresoComponent,
+    EgresoComponent,
+    FormularioComponent
   ],
   imports: [
     BrowserModule,
