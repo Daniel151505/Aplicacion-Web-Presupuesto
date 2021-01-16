959f299b33d7bf207e37a15874aaeaa414f6bd47
Daniel151505 <daniel150520011@gmail.com>
2021-01-16 15:50:56 -0500

    Creando Servicio Ingreso

---
+++ aplicacionPresupuesto/src/app/ingreso/ingreso.service.ts
@@ -0,0 +1,12 @@
+//importando servicio
+import { Ingreso } from "./ingreso.model";
+
+export class ingresoService {
+    constructor() {}
+    
+    //Creando Arreglo
+    ingresos: Ingreso[]= [
+        new Ingreso('Salario',4000),
+        new Ingreso('Venta de auto', 500)
+    ];
+}
